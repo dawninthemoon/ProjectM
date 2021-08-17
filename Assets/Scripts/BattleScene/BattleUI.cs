61edc94e34f0cfad2bd48babc39aa0d050172706
DESKTOP-JT6F47Q\nuts1 <nuts1925@naver.com>
2021-08-17 15:08:45 +0900

    Observer Pattern

---
+++ Assets/Scripts/BattleScene/BattleUI.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BattleUI : MonoBehaviour {
+    [SerializeField] TextMeshProUGUI[] _allyHPTexts = null;
+}
