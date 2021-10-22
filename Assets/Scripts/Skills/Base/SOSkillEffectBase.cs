0a0f2b024df331a10ec3333cc2de8ca111b0f2cc
sheepycrunch <dxtr126@gmail.com>
2021-10-22 22:30:37 +0900

    **************** Empty Project without Resources
    리소스빼고 나머지 삭제

--- Assets/Scripts/Skills/Base/SOSkillEffectBase.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class SOSkillEffectBase : ScriptableObject, ISkillEffect {
-    public abstract void ExecuteSkill(SkillInfo info, BattleControl battleControl);
-}
