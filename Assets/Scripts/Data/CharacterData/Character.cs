a58d2f3b6a305809d6877f80b37bd6b4ac4e963f
dawninthemoon <nuts1925@naver.com>
2021-10-26 15:28:35 +0900

    json Data 수정 및 character data에 레어도 컬럼 삭제

--- Assets/Scripts/Data/CharacterData/Character.cs
+++ Assets/Scripts/Data/CharacterData/Character.cs
@@ -50,7 +50,6 @@ namespace Data
         public EClassType ClassType;
         public EGenderType GenderType;
         public int Grade;
-        public bool Rarity;
         public string SkillCard1Key;
         public string SkillCard2Key;
         public string SpecialCardKey;
@@ -65,7 +64,6 @@ namespace Data
             ClassType = (Character.EClassType)System.Enum.Parse(typeof(Character.EClassType), jsonObj.GetString("Class"));
             GenderType = (Character.EGenderType)System.Enum.Parse(typeof(Character.EGenderType), jsonObj.GetString("Gender"));
             Grade = (int)jsonObj.GetNumber("Grade");
-            Rarity = jsonObj.GetBoolean("Rarity");
             SkillCard1Key = jsonObj.GetString("SkillCard1Key");
             SkillCard2Key = jsonObj.GetString("SkillCard2Key");
             SpecialCardKey = jsonObj.GetString("SpecialCardKey");
