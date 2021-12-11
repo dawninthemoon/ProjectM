8f5bb58a18644cfa644722c1e9a66d0f9e1a2380
hyeondo <godd7776@gmail.com>
2021-12-12 02:16:00 +0900

    코드메이드

--- Assets/Scripts/BattleScene/Base/BattleUI.cs
+++ Assets/Scripts/BattleScene/Base/BattleUI.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 using TMPro;
 using RieslingUtils;
 
-public struct BattleUIArgs {
+public struct BattleUIArgs
+{
     public float[] allyHPFillAmounts;
     public float[] enemyHPFillAmounts;
     public int currentCost;
     public int maxCost;
 
-    public BattleUIArgs(float[] ally01, float[] enemy01, int curCost, int maxCost) {
+    public BattleUIArgs(float[] ally01, float[] enemy01, int curCost, int maxCost)
+    {
         allyHPFillAmounts = ally01;
         enemyHPFillAmounts = enemy01;
         currentCost = curCost;
@@ -19,19 +21,22 @@ public struct BattleUIArgs {
     }
 }
 
-
-public class BattleUI : MonoBehaviour {
-    [SerializeField] Image[] _allyHPUIImages = null;
-    [SerializeField] Image[] _allyEXPUIImages = null;
-    [SerializeField] Image[] _enemyHPUIImages = null;
-    [SerializeField] TextMeshProUGUI _costText = null;
+public class BattleUI : MonoBehaviour
+{
+    [SerializeField] private Image[] _allyHPUIImages = null;
+    [SerializeField] private Image[] _allyEXPUIImages = null;
+    [SerializeField] private Image[] _enemyHPUIImages = null;
+    [SerializeField] private TextMeshProUGUI _costText = null;
     private static readonly string SlashString = "/";
 
-    public void OnSkillUsed(BattleUIArgs args) {
-        foreach (var ui in _allyHPUIImages) {
+    public void OnSkillUsed(BattleUIArgs args)
+    {
+        foreach (var ui in _allyHPUIImages)
+        {
             ui.transform.parent.gameObject.SetActive(true);
         }
-        foreach (var ui in _enemyHPUIImages) {
+        foreach (var ui in _enemyHPUIImages)
+        {
             ui.transform.parent.gameObject.SetActive(true);
         }
 
@@ -40,35 +45,45 @@ public class BattleUI : MonoBehaviour {
         OnCostChanged(args.currentCost, args.maxCost);
     }
 
-    private void OnAllyHPChanged(float[] args) {
-        for (int i = 0; i < _allyHPUIImages.Length; ++i) {
-            if (args == null || args.Length <= i) {
+    private void OnAllyHPChanged(float[] args)
+    {
+        for (int i = 0; i < _allyHPUIImages.Length; ++i)
+        {
+            if (args == null || args.Length <= i)
+            {
                 SetImageFillAmount(_allyHPUIImages[i], 1f);
                 SetImageFillAmount(_allyHPUIImages[i], 1f);
             }
-            else {
+            else
+            {
                 SetImageFillAmount(_allyHPUIImages[i], args[i]);
                 SetImageFillAmount(_allyHPUIImages[i], args[i]);
-            }   
+            }
         }
     }
 
-    private void OnEnemyHPChanged(float[] args) {
-        for (int i = 0; i < _enemyHPUIImages.Length; ++i) {
-            if (args == null || args.Length <= i) {
+    private void OnEnemyHPChanged(float[] args)
+    {
+        for (int i = 0; i < _enemyHPUIImages.Length; ++i)
+        {
+            if (args == null || args.Length <= i)
+            {
                 SetImageFillAmount(_enemyHPUIImages[i], 1f);
             }
-            else {
+            else
+            {
                 SetImageFillAmount(_enemyHPUIImages[i], args[i]);
-            }   
+            }
         }
     }
 
-    private void OnCostChanged(int currentCost, int maxCost) {
+    private void OnCostChanged(int currentCost, int maxCost)
+    {
         _costText.text = StringUtils.MergeStrings(currentCost.ToString(), SlashString, maxCost.ToString());
     }
 
-    private void SetImageFillAmount(Image image, float amount) {
+    private void SetImageFillAmount(Image image, float amount)
+    {
         image.fillAmount = amount;
     }
 }
